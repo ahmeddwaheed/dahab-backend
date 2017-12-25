c58f6b29fc0eca1501af0c95701c18100128a23a
hussein shalaby <drshalaby22@gmail.com>
2017-12-25 17:32:52 +0200

    pool

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171225093028) do
+ActiveRecord::Schema.define(version: 20171225151229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171225093028) do
     t.time "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "number_of_users"
     t.index ["admin_id"], name: "index_pools_on_admin_id"
   end
 
