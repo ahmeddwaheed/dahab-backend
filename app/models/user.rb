01f369fd81cbb237e2c7cc014bf55efe0903b9be
Waheed <ahmed.waheed56@gmail.com>
2017-12-25 17:42:05 +0200

    Create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
 
     has_many :requests, dependent: :destroy
-    has_many :pools
+    # has_many :pools
+    # has_many :requests
 
     validates_presence_of :username
 
     validates_presence_of :email
+    validates_presence_of :password
 
-    validates :email, uniqueness: true
+    validates_email_format_of :email, :message => 'is not looking good'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171225120353) do
+ActiveRecord::Schema.define(version: 20171225111248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password"
     t.string "email"
     t.boolean "in_pool", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
   end
 
 end
