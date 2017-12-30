e2dcea88e8d5ad353bc7b335fd8c792308144302
hussein shalaby <drshalaby22@gmail.com>
2017-12-30 21:02:05 +0200

    adding admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171226120519) do
+ActiveRecord::Schema.define(version: 20171230155825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pools", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,12 @@
 # pools = Pool.create!([{name: 'GearUp2017', amount: '40000', monthly_amount: '4000', seat_number: '10', status: 'comming' },
 #         {name: 'FireUp2017', amount: '200000', monthly_amount: '20000', seat_number: '10', status: 'running' },
 #         {name: 'FireUp2016', amount: '4000', monthly_amount: '500', seat_number: '8', status: 'running' }])
-
+#
 # users = User.create!([{username: 'hussien', password: '123456', email: 'hussien@me.com'},
 #         {username: 'Waheed', password: '123456', email: 'Waheed@me.com'},
 #         {username: 'Mostafa', password: '123456', email: 'mostafa@me.com'}])
-
-requests = Request.create!([{background: "ay klam", program: "fireup", reason:"klam brdo", user_id: 35, pool_id: 6, is_accepted:true}])
-
-
+#
+# requests = Request.create!([{background: "ay klam", program: "fireup", reason:"klam brdo", user_id: 35, pool_id: 6, is_accepted:true}])
 
 
-
-
+admins = Admin.create!([{name: "hussein", email: "hussein@almakinah.com", password: "123456789"}])
